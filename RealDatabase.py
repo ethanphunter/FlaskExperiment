83e617508d0edc407c87eea6cbf688252f1af06a
Ethan Hunter <ethan.hunter@banno.com>
2016-07-06 18:51:38 -0500

    Trying to get tests to pass

--- RealDatabase.py
+++ RealDatabase.py
@@ -47,8 +47,8 @@ class Database():
     def setUpTestDb(self,y):
         self.writeQuery("create table users (username text, password text, games text, friends text, friend_requests text)")
         self.writeQuery("create table games (game_id text, game_data json, players text, turn text)")
-        self.writeQuery("""insert into users values ('test1','{}')""".format(y[0]))
-        self.writeQuery("""insert into users values ('test2','{}')""".format(y[1]))
+        self.writeQuery("""insert into users (username, password) values ('test1','{}')""".format(y[0]))
+        self.writeQuery("""insert into users (username, password) values ('test2','{}')""".format(y[1]))
 
     def getById(self,id):
         return self.getQuery("""select * from inventory where id = '{}'""".format(id))
