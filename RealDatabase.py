bda8666babd59f6186063e6cc72683a482ada068
Ethan Hunter <ethanphunter@users.noreply.github.com>
2016-07-06 20:29:18 -0500

    Update RealDatabase.py

--- RealDatabase.py
+++ RealDatabase.py
@@ -1,4 +1,4 @@
-from psycopg2.extras import Json
+from psycopg2.extras import Json, register_json
 from SecretGenerator import getDatabaseUrl
 from Utilities import listToCsvString
 import datetime
@@ -46,10 +46,11 @@ class Database():
 
     def setUpTestDb(self,y):
         from UserLoginPackage import encryptString
-        print(self.writeQuery("create table users (username text, password text, games text, friends text, friend_requests text)"))
-        print(self.writeQuery("create table games (game_id text, game_data json, players text, turn text)"))
-        print(self.writeQuery("""insert into users (username, password) values ('test1','{}')""".format(encryptString(y[0]))))
-        print(self.writeQuery("""insert into users (username, password) values ('test2','{}')""".format(encryptString(y[1]))))
+        register_json(self.conn)
+        self.writeQuery("create table users (username text, password text, games text, friends text, friend_requests text)")
+        self.writeQuery("create table games (game_id text, game_data text, players text, turn text)")
+        self.writeQuery("""insert into users (username, password) values ('test1','{}')""".format(encryptString(y[0])))
+        self.writeQuery("""insert into users (username, password) values ('test2','{}')""".format(encryptString(y[1])))
 
     def getById(self,id):
         return self.getQuery("""select * from inventory where id = '{}'""".format(id))
