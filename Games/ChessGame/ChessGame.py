41bbb283bc8e531d1c96c843e1c6f3b675f24f7f
Ethan Hunter <ethan.hunter@banno.com>
2016-07-20 20:52:12 -0500

    Darn variable shadowing

--- Games/ChessGame/ChessGame.py
+++ Games/ChessGame/ChessGame.py
@@ -1,72 +1,10 @@
 """a user will have a list of game ids that they will be a part of
     a game will have an id, two usernames, whose turn it is, and the board state"""
 
-"""
-TODO:
-Work on storing the game in the db
-"""
-
-from Games import Game
+from Games.Game import Game
 from ChessPieces import *
 from ChessBoard import *
 from flask import request, redirect, session, abort
-
-# columnLetters = ["a","b","c","d","e","f","g","h"]
-
-# def newBoard():
-#     x = []
-#     for row in range(0,8):
-#         y = []
-#         for col in range(0,8):
-#             y.append("")
-#         x.append(y)
-#     return x
-#
-# def setupBoard():
-#     board = newBoard()
-#     for row in range(0,8):
-#         if (row == 1):
-#             for col in range(0,8):
-#                 board[row][col] = Pawn(row,col,"black")
-#         elif (row == 6):
-#             for col in range(0,8):
-#                 board[row][col] = Pawn(row,col,"white")
-#     return board
-#
-# def newGame():
-#     session["board"] = stringifyBoard(setupBoard())
-#
-# def stringifyBoard(board):
-#     stringifiedBoard = []
-#     for row in board:
-#         stringedRow = []
-#         for col in row:
-#             stringedRow.append(str(col))
-#         stringifiedBoard.append(stringedRow)
-#     return stringifiedBoard
-#
-# def unStringifyBoard(board):
-#     unStringifiedBoard = []
-#     rowI = 0
-#     for row in board:
-#         unStringedRow = []
-#         colI = 0
-#         for col in row:
-#             if ("Pawn" in col):
-#                 unStringedRow.append(Pawn(rowI,colI,col.split(" ")[0]))
-#             else:
-#                 unStringedRow.append("")
-#             colI += 1
-#         rowI += 1
-#         unStringifiedBoard.append(unStringedRow)
-#     calculateValidMoves(unStringifiedBoard)
-#     return unStringifiedBoard
-#
-# def calculateValidMoves(board):
-#     for row in board:
-#         for col in row:
-#             if (col != ""):
-#                 col.calculateValidSpaces(board)
 
 
 class ChessGame(Game):
